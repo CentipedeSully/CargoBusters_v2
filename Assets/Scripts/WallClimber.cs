2890b1e1201ba5c145b4605d1aaba9e936b5518d
William Sullivan-Smith <sullivansmith057@gmail.com>
2025-06-08 06:00:38 -0500

    new wall detection is implemneted. Less systems to maintain, and debug visuals (physics analytics) are accurate. Basic wall detection happens first, then an advanced analysis is performed if something is detected. Each point is tested for 'standability', meaning more casts are done to check if the point is a ledge or not. Also a debug system exists to show the exact impact points and valid stand position for easier debugging. Very successful day.

--- Assets/Scripts/WallClimber.cs
+++ Assets/Scripts/WallClimber.cs
@@ -16,27 +16,34 @@ public class WallClimber : MonoBehaviour
     [SerializeField] private float _capsuleRadius = .5f;
     private int _detectionArraySize = 10;
 
+    [Header("Debug Visuals")]
+    [SerializeField] private bool _showDebug = false;
+    [SerializeField] private GameObject _debugMarkerPrefab;
+    [SerializeField] private GameObject _standableMarkerPrefab;
+    [SerializeField] private Transform _debugMarkersContainer;
+    [SerializeField] private int _maxMarkers = 20;
+    private List<GameObject> _debugMarkers = new();
+    private List<GameObject> _debugStandableMarkers = new();
+ 
     [Header("Forwards Wall Detection")]
+    [Tooltip("Wall casting scans forwards in steps, from the player's max ledge reach down to a specified cutoff." +
+        "Smaller values improve accuracy at the cost of performance. This calculation happens many times each frame " +
+        "if a wall has been detected.")]
+    [SerializeField] private float _verticalCastStepSize = .05f;
+    [Tooltip("Wall casting stops once this position is passed over." +
+        "Useful to ignore easily-traverseable low terrain")]
+    [SerializeField] private Transform _lowestWallDetectionBound;
     [SerializeField] private float _forwardsWallCastDistance;
     [SerializeField] private Vector3 _detectedWallPoint = Vector3.negativeInfinity;
     private RaycastHit[] _wallDetectionResults;
 
-    [Header("standable Ledge Detection")]
+    [Header("Standable Ledge Detection")]
+    [Tooltip("The necessary space upwards that the standCast will be performed from the potential ledge point.")]
+    [SerializeField] private float _verticalLedgeSpacing = 0.1f;
+    [Tooltip("The space forwards ONTO THE LEDGE that the standCast will be performed from the potential ledge point." +
+        "Emulates how far the player would transition onto the ledge")]
+    [SerializeField] private float _OntoLedgeSpacing = 0.1f;
 
-    [Tooltip("The highest point that a standable position may be checked." +
-        " No standablility checks are made beyond this height. The cast's size is" +
-        "the player's bottom + top capsule by the capsule's radius")]
-    [SerializeField] private Transform _highestStandableOrigin;
-    
-    [Tooltip("The lowest point that a standable position may be checked. " +
-        "Any detections that're under this point will be ignored. They can be " +
-        "navigated via normal movement")]
-    [SerializeField] private Transform _lowestLedgeOrigin;
-    
-    [Tooltip("how far the cast should step up the detected wall to find a standable position. " +
-        "Smaller values improve accuracy at the cost of performance. This calculation happens every frame " +
-        "if a wall has been detected.")]
-    [SerializeField] private float _castStepSize = .05f;
 
     private RaycastHit[] _ledgeDetectionResults;
 
@@ -45,12 +52,22 @@ public class WallClimber : MonoBehaviour
 
 
     //Monobehaviours
+
+    private void Start()
+    {
+        PopulateDebugMarkers();
+    }
+
     private void Update()
     {
         if (_isWallClimbingEnabled)
         {
-            DetectWall();
-            FindStandableLedge();
+            if (_showDebug)
+                ClearMarkers();
+
+            if (IsObstacleDetected())
+                CreateForwardsObstacleAnalysis();
+            
         }
             
     }
@@ -59,24 +76,136 @@ public class WallClimber : MonoBehaviour
 
 
     //Internals
-    private void DetectWall()
+    private void PopulateDebugMarkers()
     {
-        _wallDetectionResults = new RaycastHit[_detectionArraySize];
+        if (_showDebug)
+        {
+            //create a large amount of wall detection markers
+            int markerCount = _debugMarkers.Count;
+            GameObject latestCreatedObject;
+            while (markerCount < _maxMarkers)
+            {
+                latestCreatedObject = Instantiate(_debugMarkerPrefab, _debugMarkersContainer);
+                latestCreatedObject.SetActive(false);
+                _debugMarkers.Add(latestCreatedObject);
+                markerCount++;
+            }
 
-        //Wall Detect forwards from the player's feet to the player's highest ledge-grab reach
-        Physics.CapsuleCastNonAlloc(_maxLedgeReachOrigin.position,_bottomPlayerCapsuleOrigin.position, _capsuleRadius, transform.TransformDirection(Vector3.forward), _wallDetectionResults, _forwardsWallCastDistance, _wallLayers);
-
-        //Get the detection that's closest to our origin
-        _detectedWallPoint = Utils.GetClosestPoint(_wallCastOrigin.position, _wallDetectionResults);
+            //create a few standable markers
+            for (int count = 0; count < 3; count++)
+            {
+                latestCreatedObject = Instantiate(_standableMarkerPrefab, _debugMarkersContainer);
+                latestCreatedObject.SetActive(false);
+                _debugStandableMarkers.Add(latestCreatedObject);
+            }
+        }
+        
     }
 
-    private void FindStandableLedge()
+    private void ClearMarkers()
     {
-        //validate a wall has been detected
-        if (_detectedWallPoint != Vector3.negativeInfinity)
+        foreach (GameObject marker in _debugMarkers)
+            marker.SetActive(false);
+
+        foreach (GameObject marker in _debugStandableMarkers)
+            marker.SetActive(false);
+    }
+
+    private void WallCastForwards(Vector3 start, float size)
+    {
+        _wallDetectionResults = new RaycastHit[_detectionArraySize];
+        Physics.SphereCastNonAlloc(start, size, transform.TransformDirection(Vector3.forward), _wallDetectionResults, _forwardsWallCastDistance, _wallLayers);
+    }
+
+    
+    private bool IsObstacleDetected() //Performs a cheap cast to determine if we need to build an obstacle analysis 
+    {
+        return Physics.CapsuleCast(_maxLedgeReachOrigin.position, _bottomPlayerCapsuleOrigin.position, _capsuleRadius, transform.TransformDirection(Vector3.forward), _forwardsWallCastDistance, _wallLayers);
+    }
+
+    private void CreateForwardsObstacleAnalysis() //performs a relatively expensive analysis of any obstacles ahead of the entitiy
+    {
+        
+
+        //Start detecting walls from the highest point first
+        Vector3 currentCastOrigin = _maxLedgeReachOrigin.position;
+        int iterationCount = 1;
+
+        while (currentCastOrigin.y > _lowestWallDetectionBound.position.y)
         {
+            //perform the cast
+            WallCastForwards(currentCastOrigin, _capsuleRadius);
+
+            //Get the detection that's closest to our origin
+            _detectedWallPoint = Utils.GetClosestPoint(_wallCastOrigin.position, _wallDetectionResults);
+
             
+
+            if (_showDebug)
+            {
+                //tag the hit with a debug marker, if it's a valid detection
+                if (_detectedWallPoint.x != float.NegativeInfinity &&               //existence check
+                    _detectedWallPoint.y >= _lowestWallDetectionBound.position.y && //lower bound check
+                    _detectedWallPoint.y <= _maxLedgeReachOrigin.position.y)     //upper bound check
+                {
+                    //make sure we have enough markers.
+                    if (iterationCount <= _debugMarkers.Count)
+                    {
+                        //tag the collision point with a marker
+                        _debugMarkers[iterationCount - 1].transform.position = _detectedWallPoint;
+                        _debugMarkers[iterationCount - 1].SetActive(true);
+                    }
+
+                    //test the point for standability
+                    bool isPointStandable = TestPointForStandability(_detectedWallPoint);
+                    
+                    
+                    if (isPointStandable)
+                    {
+                        //apply spacing to the point
+                        Vector3 position = _detectedWallPoint + (Vector3.up * _verticalLedgeSpacing) + (transform.TransformDirection(Vector3.forward * _OntoLedgeSpacing));
+
+                        //offset the point by half the player's height
+                        position += Vector3.up * ((_topPlayerCapsuleOrigin.position.y - _bottomPlayerCapsuleOrigin.position.y)/2 + _capsuleRadius);
+
+                        _debugStandableMarkers[0].transform.position = position;
+                        _debugStandableMarkers[0].SetActive(true);
+                    }
+                    
+                }
+            }
+            
+
+            //setp the cast origin down for the next cast
+            currentCastOrigin.y -= _verticalCastStepSize;
+
+            //increment iteration count, used by the debug marker collection
+            iterationCount++;
+
         }
+
+    }
+
+    private bool TestPointForStandability(Vector3 point)
+    {
+        //apply the spacing
+        point += Vector3.up * _verticalLedgeSpacing;
+        point += transform.TransformDirection(Vector3.forward * _OntoLedgeSpacing);
+
+        Vector3 FeetPosition = point + (Vector3.up * _capsuleRadius);
+        Vector3 TopPosition = point + (Vector3.up * (_topPlayerCapsuleOrigin.position.y - _bottomPlayerCapsuleOrigin.position.y + _capsuleRadius));
+
+        //create an offset cast
+        Collider[] hits = Physics.OverlapCapsule(FeetPosition, TopPosition, _capsuleRadius, _wallLayers);
+
+        if (hits.Length == 0)
+            return true;
+        else return false;
+    }
+
+    private void TestPointForLedgeability(Vector3 point)
+    {
+
     }
 
 
